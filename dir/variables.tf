5e7f51b7d96a663a1e234996613a1feee20fda1f
jiseon.kim <jiseon.kim@bespinglobal.com>
2022-12-01 14:58:59 +0900

    Add dir

---
+++ dir/variables.tf
@@ -0,0 +1,19 @@
+variable "region" {
+  default = "KR"
+}
+
+variable "subnet_no" {
+  default = "66177"
+}
+
+variable "server_name" {
+  default = "hi-terraform-test"
+}
+
+variable "server_image_product_code" {
+  default = "SW.VSVR.OS.LNX64.CNTOS.0703.B050"
+}
+
+variable "server_login_key_name" {
+  default = "ncp-demo-ssh-key"
+}
